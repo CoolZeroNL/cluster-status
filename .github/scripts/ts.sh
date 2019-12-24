72fc6dbfb1d556f8259ec27ee34c425602a792fb
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-24 21:48:42 +0100

    Update ts.sh

--- .github/scripts/ts.sh
+++ .github/scripts/ts.sh
@@ -10,10 +10,32 @@ set -e
 _host=${1:-'http://www.google.com'}
 _port=${2:-'10011'}
 
-  echo -e "use \nquit" | \
-  nc $_host $_port | tr -d "\r" | {
-    while read -r line; do
-      echo "$line"
-    done
-  } || return 1
+while read line
+do
+    if [ "$line" == 'exit' ]; then
+        echo "Received 'exit'"
+        break
+    else
+        echo "$line"
+    fi
+done < <((echo -e "use \nquit") | nc -q -1 "$_host" "$_port")
+
+if [ -z "$line" ]
+then
+      echo "$_host is down"
+        #down
+        exit 1
+else
+      echo "$_host is up"
+        # up
+        exit 0
+fi
 exit 0
+
+#   echo -e "use \nquit" | \
+#   nc $_host $_port | tr -d "\r" | {
+#     while read -r line; do
+#       echo "$line"
+#     done
+#   } || return 1
+# exit 0
