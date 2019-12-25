cb236cd0b97bf8ddf786791f9120cefd0c3e1f17
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-25 13:55:21 +0100

    Update minecraft-local-version-check.sh

--- .github/scripts/minecraft-local-version-check.sh
+++ .github/scripts/minecraft-local-version-check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-folder='minecraft-00'
+_folder=${1:-'minecraft-01'}
 
 sudo cat /home/minecraft/deployed/$_folder/logs/latest.log | grep net.minecraftforge.fml.VersionChecker/ | grep 'Found status:'
