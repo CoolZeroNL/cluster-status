6087cd4f98f4b63d16945362e4b1247122cc68c2
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-25 14:06:55 +0100

    Update minecraft-local-version-check.sh

--- .github/scripts/minecraft-local-version-check.sh
+++ .github/scripts/minecraft-local-version-check.sh
@@ -2,4 +2,5 @@
 
 _folder=${1:-'minecraft-01'}
 
-sudo cat /home/minecraft/deployed/$_folder/logs/latest.log | grep net.minecraftforge.fml.VersionChecker/ | grep 'Found status:'
+currentt=`sudo cat /home/minecraft/deployed/$_folder/logs/latest.log | grep net.minecraftforge.fml.VersionChecker/ | grep 'Found status:'  | awk -F': ' '{print $4}' | awk -F' ' '{print $1}'`
+target=`sudo cat /home/minecraft/deployed/$_folder/logs/latest.log | grep net.minecraftforge.fml.VersionChecker/ | grep 'Found status:'  | awk -F': ' '{print $5}'`
