0aa940fd326aca81070f381c8237b915c0f2d7a0
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-25 12:53:48 +0100

    Update minecraft-version-check.sh

--- .github/scripts/minecraft-version-check.sh
+++ .github/scripts/minecraft-version-check.sh
@@ -73,7 +73,6 @@
                 sed -i 's/FORGEVER=.*/FORGEVER='$file_FORGEVER'/g' settings.cfg
                 sed -i 's/MCVER=.*/MCVER='$file_MCVER'/g' settings.cfg
                 ls -la
-                env
                 # git update
                 git config --global user.name MinecraftAutoUpdater
                 git config --global user.email mincecraft@legendsandmasters.nl
