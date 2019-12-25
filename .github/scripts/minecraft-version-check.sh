52e2712df934897f09791c5523e2a0b07488a554
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2019-12-25 11:20:52 +0100

    Update minecraft-version-check.sh

--- .github/scripts/minecraft-version-check.sh
+++ .github/scripts/minecraft-version-check.sh
@@ -16,43 +16,43 @@
         MCVER=`cat $folder/settings.cfg | grep MCVER | awk -F'=' '{print $2}'`
         FORGEVER=`cat $folder/settings.cfg | grep FORGEVER | awk -F'=' '{print $2}'`
 
-        echo "MCVER "$MCVER
-        echo "FORGEVER "$FORGEVER
+        # echo "MCVER "$MCVER
+        # echo "FORGEVER "$FORGEVER
         
         # Get versions from page
         cur_site_version=`curl -qs https://files.minecraftforge.net/maven/net/minecraftforge/forge/ | grep 'Downloads for Minecraft Forge - MC' | grep -oP '(?<=<h1>).*?(?=</h1>)' | awk -F'Downloads for Minecraft Forge - MC ' '{print $2}'`
-        echo "PAGEVER "$cur_site_version
+        # echo "PAGEVER "$cur_site_version
         
         if verlte $cur_site_version $MCVER; then
         # MCVER is bigger then cur_site_version	
-        echo "true"
+        # echo "true"
 
         latest_MCVER_on_site=`curl -qs https://files.minecraftforge.net/maven/net/minecraftforge/forge/ | grep -m 1 '/maven/net/minecraftforge/forge/index_' | grep -oP -m 1 '(?<=.html">).*?(?=</a>)' | grep ''`
         # latest_MCVER_on_site=`curl -qs https://files.minecraftforge.net/maven/net/minecraftforge/forge/ | grep '/maven/net/minecraftforge/forge/index_' | head -n1 `
-        echo "LATEST "$latest_MCVER_on_site
+        # echo "LATEST "$latest_MCVER_on_site
 
         # lates MCVER URL page.
         latest_MCVER_url=`curl -qs https://files.minecraftforge.net/maven/net/minecraftforge/forge/ | grep -m 1 '/maven/net/minecraftforge/forge/index_' | sed -n 's/.*href="\([^"]*\).*/\1/p'`
-        echo "URL "$latest_MCVER_url
+        # echo "URL "$latest_MCVER_url
 
         # get url lastest on page
         latest_installer_url=`curl -sq https://files.minecraftforge.net/$latest_MCVER_url | grep '(Direct Download)' | grep installer | sed -n 's/.*href="\([^"]*\).*/\1/p' | head -n1`
-        echo $latest_installer_url
+        # echo $latest_installer_url
         
         full_filename=$(basename -- "$latest_installer_url")
         extension="${full_filename##*.}"
         filename="${full_filename%.*}"
 
-        echo $filename
-        echo $full_filename
-        echo $extension
+        #echo $filename
+        #echo $full_filename
+        #echo $extension
 
         file_MCVER=`echo $filename | awk -F'-' '{print $2}'`
         file_FORGEVER=`echo $filename | awk -F'-' '{print $3}'`
 
-        echo $file_MCVER
-        echo $file_FORGEVER
-        echo $file_MCVER '-' $MCVER 
+        #echo $file_MCVER
+        #echo $file_FORGEVER
+        echo $file_MCVER '< - >' $MCVER 
 
         if verlte $MCVER $file_MCVER || verlte $file_MCVER $MCVER; then
             # echo "MCFILE eq or file is higher"
@@ -61,8 +61,7 @@
 
               if verlt $FORGEVER $file_FORGEVER; then
               echo "file forgever is heigher or EQ"
-              echo $file_FORGEVER
-              echo $FORGEVER
+              echo $file_FORGEVER "< - >" $FORGEVER
               exit 2
               # sed -i 's/FORGEVER=.*/FORGEVER='$file_FORGEVER'/g' settings.cfg
               # sed -i 's/MCVER=.*/MCVER='$file_MCVER'/g' settings.cfg
